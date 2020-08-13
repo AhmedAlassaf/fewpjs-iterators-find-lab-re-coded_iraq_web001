04bb54d988158ffd1f0ac71a8670f4eb12aeb96b
Ahmed Alassaf <ahmedfaysal199@gmail.com>
2020-08-13 19:44:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let winCheck = arr.find(el => el.result === "W")
+  return !!winCheck ? winCheck.year : undefined;
+}
